f9b63b7e08d463b782d5172ce2c45f0292cf3d35
Duy Vu <dtv15@imperial.co.uk>
2017-06-09 12:43:57 +0100

    Added travis.yml & Test files in src

---
+++ src/test/modelTest.js
@@ -0,0 +1,46 @@
+const User = require('/models/user');
+const config = require('../config/database');
+const expect = require('chai').expect
+
+
+describe('User module', () => {  
+  describe('"getUserById"', () => {
+    it('should export a function', () => {
+      expect(User.getUserById).to.be.a('function')
+    })
+  })
+})
+//
+//describe('User Model', function() {
+//  it('')
+//});
+
+const userArray = [
+  {
+    name: 'Michael',
+    email: 'michael@mherman.org',
+    username: 'michael',
+    password: '123456789'
+  },
+  {
+    name: 'Bob',
+    email: 'bob@mherman.org',
+    username: 'bob',
+    password: 'abcdefghi'
+  },
+  {
+    name: 'Alice',
+    email: 'alice@mherman.org',
+    username: 'alice',
+    password: 'aliceAwesome'
+  }
+]
+//;
+//it('should return all users created on or after (>=) specified year',
+//(done) => {
+//  usersController.filterByYear(userArray, 2015, (err, total) => {
+//    should.not.exist(err);
+//    total.length.should.eql(2);
+//    done();
+//  });
+//});
