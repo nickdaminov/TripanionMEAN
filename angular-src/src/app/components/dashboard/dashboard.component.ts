a1868cd8c9f34bcb862198ee5af39264394c1420
ak8815 <ak8815@ic.ac.uk>
2017-06-16 09:35:22 +0100

    I have Implemented room pages

--- angular-src/src/app/components/dashboard/dashboard.component.ts
+++ angular-src/src/app/components/dashboard/dashboard.component.ts
@@ -7,6 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
+destination:String;
+trips = [{
+destination:"dsds",
+likes:12,
+
+going:12
+},
+{
+destination:"dsds",
+likes:12,
+going:12
+}
+];
+
+click(index) {
+  this.trips[index].likes = this.trips[index].likes + 1;
+}
+
   constructor() { }
 
   ngOnInit() {
