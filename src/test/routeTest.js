c1bbd3c26c5f90b9ea8973229817ce9ee2e75ab5
Duy Vu <dtv15@imperial.co.uk>
2017-06-16 15:18:25 +0100

    Added node_module in gitignore; Added routeTest.js

---
+++ src/test/routeTest.js
@@ -0,0 +1,21 @@
+// var request = require('supertest');
+// require = require('really-need');
+// describe('loading express', function () {
+//   var server;
+//   beforeEach(function () {
+//     server = require('../routes/users', { bustCache: true });
+//   });
+//   afterEach(function (done) {
+//     server.close(done);
+//   });
+//   it('responds to /', function(done) {
+//   request(server)
+//     .get('/')
+//     .expect(200);
+  });
+  // it('404 everything else', function(done) {
+  //   request(server)
+  //     .get('/foo/bar')
+  //     .expect(404, done);
+  // });
+});
